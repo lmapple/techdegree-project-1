8c2991140d7b212b9ac6b7e6b9dbf69e0c6ee3ed
lmapple <46875633+lmapple@users.noreply.github.com>
2019-01-27 17:00:29 -0700

    Delete out unnecessary comments and adjust spacing

--- techdegree.project_1.guessing_game.py
+++ techdegree.project_1.guessing_game.py
@@ -1,17 +1,13 @@
 """
+lmapple
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
-
 import random
-
 high_score = [0]
+
+
 def new_high_score(number_of_guesses):
   high_score.append(number_of_guesses)
   if high_score[0] == 0:
@@ -21,26 +17,8 @@ def new_high_score(number_of_guesses):
   elif high_score[0] > high_score[1]:
     high_score.pop(0)
     
-
+    
 def start_game():
-  """Psuedo-code Hints
-  
-  When the program starts, we want to:
-  ------------------------------------
-  1. Display an intro/welcome message to the player.
-  2. Store a random number as the answer/solution.
-  3. Continuously prompt the player for a guess.
-    a. If the guess greater than the solution, display to the player "It's lower".
-    b. If the guess is less than the solution, display to the player "It's higher".
-  
-  4. Once the guess is correct, stop looping, inform the user they "Got it"
-       and show how many attempts it took them to get the correct number.
-  5. Let the player know the game is ending, or something that indicates the game is over.
-  
-  ( You can add more features/enhancements if you'd like to. )
-  """
-  # write your code inside this function.
-
   print("Welcome to the Number Guessing Game!")
   print("Play by guessing a number between 1 and 10. If you guess right, you win!")
   solution = random.randint(1,10)
@@ -89,6 +67,6 @@ def start_game():
       play_again = input("Please choose 'Y' for yes or 'N' for no. >>  ")
       continue
   
+  
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
-    start_game() 
+  start_game() 
